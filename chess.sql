fa72f0b71abaf9d4bc0d022ec119912066d58a43
Ivanpillow <diego.hernandez5523@alumnos.udg.mx>
2024-11-25 01:06:56 -0600

    Actualizacion BD

--- chess.sql
+++ chess.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-11-2024 a las 02:04:56
+-- Tiempo de generación: 25-11-2024 a las 08:06:06
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -42,7 +42,25 @@ CREATE TABLE `games` (
 INSERT INTO `games` (`game_id`, `player_id_white`, `player_id_black`, `winner_id`, `game_date`) VALUES
 (40, 7, 9, NULL, '2024-11-24'),
 (41, 9, 8, NULL, '2024-11-24'),
-(42, 9, 7, NULL, '2024-11-24');
+(42, 9, 7, NULL, '2024-11-24'),
+(43, 7, 8, NULL, '2024-11-24'),
+(44, 8, 7, 8, '2024-11-24'),
+(45, 7, 8, NULL, '2024-11-24'),
+(46, 8, 7, 8, '2024-11-24'),
+(47, 7, 8, NULL, '2024-11-24'),
+(48, 8, 7, 8, '2024-11-24'),
+(49, 8, 7, 8, '2024-11-24'),
+(50, 8, 7, 8, '2024-11-24'),
+(51, 8, 7, 8, '2024-11-24'),
+(52, 8, 7, 8, '2024-11-24'),
+(53, 7, 8, NULL, '2024-11-24'),
+(54, 7, 8, NULL, '2024-11-24'),
+(55, 7, 8, NULL, '2024-11-24'),
+(56, 8, 7, 8, '2024-11-24'),
+(57, 7, 8, NULL, '2024-11-25'),
+(58, 8, 7, 8, '2024-11-25'),
+(59, 8, 7, 8, '2024-11-25'),
+(60, 8, 7, NULL, '2024-11-25');
 
 -- --------------------------------------------------------
 
@@ -77,9 +95,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `score`) VALUES
-(7, 'angel', 'angel@gmail.com', '1234', 0),
-(8, 'ivan', 'ivan@gmail.com', '1234', 0),
-(9, 'chava', 'chava@gmail.com', '1234', 0);
+(7, 'angel', 'angel@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 0),
+(8, 'ivan', 'ivan@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 0),
+(9, 'chava', 'chava@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 0),
+(12, 'profe', 'profe@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 0);
 
 --
 -- Índices para tablas volcadas
@@ -118,7 +137,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `games`
 --
 ALTER TABLE `games`
-  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de la tabla `moves`
@@ -130,7 +149,7 @@ ALTER TABLE `moves`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
